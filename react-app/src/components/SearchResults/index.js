4bc25d1349cd92e220c4cb407b6fbbe3b3784f44
Marc <meir.guggenheim@gmail.com>
2023-04-23 22:09:40 -0400

    styling

--- react-app/src/components/SearchResults/index.js
+++ react-app/src/components/SearchResults/index.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { useParams } from "react-router-dom"
+import { NavLink, useParams } from "react-router-dom"
 import { searchSubs } from "../../store/search"
 
 
@@ -26,6 +26,11 @@ function SearchResults() {
     return (
         <>
         <h1>Search Results</h1>
+        <div>
+            {Object.values(subs).map(sub => (
+                <NavLink to={`/subcrudits/${sub.id}`}>{sub.name}</NavLink>
+            ))}
+        </div>
         </>
     )
 }
