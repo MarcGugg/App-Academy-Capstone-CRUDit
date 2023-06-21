91986040c2a4e740284a8a60b8c08dc97292ca09
Marc <meir.guggenheim@gmail.com>
2023-06-21 16:37:31 -0400

    all CRUD routes for comments written

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -3,6 +3,7 @@ from flask_login import current_user, login_required
 from app.models import  User, PostImage, db, Subcrudit, Post
 from app.models.comment import Comment
 from app.forms.create_comment import CreateCommentForm
+from app.forms.edit_comment import EditCommentForm
 from app.forms.create_post import CreatePostForm
 from app.forms.edit_post import EditPostForm
 
@@ -16,3 +17,80 @@ def get_all_comments():
         return [comment.to_dict_inclusive() for comment in comments]
     
     return None
+
+@comment_routes.route('/<int:comment_id>', methods=['GET', 'PUT', 'DELETE'])
+@login_required
+def get_comment_by_id(comment_id):
+    
+    comment = Comment.query.get(comment_id)
+
+    if comment:
+        if request.method == 'GET':
+            return comment.to_dict_inclusive()
+    
+        if request.method == 'PUT':
+            form = EditCommentForm()
+
+            form['csrf_token'].data = request.cookies['csrf_token']
+
+            if current_user.is_authenticated:
+
+                if comment.author_id == current_user.id:
+
+                    if form.validate_on_submit():
+
+                        data = form.data
+
+                        comment.text = data['text']
+
+                        db.session.commit()
+
+                        return comment.to_dict_inclusive()
+                
+                return {'User did not write this comment', 400}
+            
+            return {'User must log in', 400}
+        
+        if request.method == 'DELETE':
+            db.session.delete(comment)
+            db.session.commit()
+            return {'Comment Deleted'}
+        
+    return None
+
+
+@comment_routes.route('/<int:post_id>', methods=['GET', 'POST'])
+@login_required
+def get_comments_by_post_id(post_id):
+    if request.method == 'GET':
+        comments = Comment.query.filter(post_id == post_id).all()
+
+        if comments:
+            return [comment.to_dict_inclusive() for comment in comments]
+        return None
+    
+    if request.method == 'POST':
+        
+        if current_user.is_authenticated:
+            form = CreateCommentForm()
+
+            form['csrf_token'].data = request.cookies['csrf_token']
+
+            if form.validate_on_submit():
+                data = form.data
+
+                new_comment = Comment(
+                    author_id = current_user.id,
+                    text = data['text'],
+                    post_id = post_id
+                )
+
+                db.session.add(new_comment)
+                db.session.commit()
+            
+            return {'Form validation failed', 400}
+        
+        return {'User must be logged in', 400}
+    
+
+
