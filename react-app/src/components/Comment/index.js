17010dc847ef01b372b9571b12ad4708ac4f8a81
Marc <meir.guggenheim@gmail.com>
2023-07-25 20:24:07 -0400

    styling for edit comment

--- react-app/src/components/Comment/index.js
+++ react-app/src/components/Comment/index.js
@@ -75,23 +75,23 @@ function Comment({comment}) {
                     title="February 8th, 2022">Feb. 8, 2022</time></p> */}
         </div>
         </footer>
-        <textarea class="text-gray-500 dark:text-gray-400" value={newText} onChange={(e) => setNewText(e.target.value)}>{comment.text}</textarea>
+        <textarea class="w-full h-40 py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700" value={newText} onChange={(e) => setNewText(e.target.value)}>{comment.text}</textarea>
         <div class="flex items-center mt-4 space-x-4">
-        <button type="button"
+        {/* <button type="button"
             class="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400">
             <svg aria-hidden="true" class="mr-1 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
             Reply
-        </button>
+        </button> */}
             <div>
-                    {user ? user.id === comment.authorId ? 
+                    {/* {user ? user.id === comment.authorId ? 
                     // <button onClick={handleCommentDelete}>Delete</button> //RENDER THIS BUTTON IN A CONTEXT COMPONENT. PASS IN COMMENT ID
                     <CommentDeleteButton comment={comment} />
                     : '' : ''}
                 {user ? user.id === comment.authorId ?
                 <NavLink to={`/comments/${comment.id}/edit`} style={{textDecoration: 'none' , color: 'darkgray', fontWeight: '500'}}>Edit</NavLink>
-                : '' : ''}
-                <button onClick={() => setEditMode(!editMode)}>Cancel</button>
-                <button onClick={handleEditSubmit}>Submit</button>
+                : '' : ''} */}
+                <button onClick={() => setEditMode(!editMode)} class='inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-red-400 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800'>Cancel</button>
+                <button onClick={handleEditSubmit} class='inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-cyan-400 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800'>Submit</button>
             </div>
         </div>
         </article>
