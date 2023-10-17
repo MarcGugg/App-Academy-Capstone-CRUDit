e0649bcbb78ee7f81e2cc0ddd0dd79bd13ab2110
Marc <meir.guggenheim@gmail.com>
2023-10-17 17:29:37 -0400

    added more margin to comment input

--- react-app/src/components/Post/index.js
+++ react-app/src/components/Post/index.js
@@ -149,7 +149,7 @@ function OnePost() {
 
             {user ? 
                 <div>
-                      <div class="max-w-2xl mx-auto px-4">
+                      <div class="max-w-2xl mx-auto px-4 mt-40">
                         <div class="flex justify-between items-center mb-6">
                          <h2 class="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">Comments</h2>
                         </div>
